cc2a441d16778e1de575ffd5d4ad7ab43503560e
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2023-03-10 10:19:31 +0700

    Add sign up

--- projects/base-area/src/app/dto/user/login-req.ts
+++ projects/base-area/src/app/dto/user/login-req.ts
@@ -1,4 +1,4 @@
 export interface LoginReq{
-    emaiL:string, 
-    password: string, 
+    email:string,
+    password: string,
 }
--- projects/main-area/src/app/app.module.ts
+++ projects/main-area/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { ShareModule } from 'projects/base-area/src/app/share.module';
 import { AppComponent } from './app.component';
 import { AppRouting } from './app.routing';
 import { LoginComponent } from './pages/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { CodeInputModule } from 'angular-code-input';
 
 @NgModule({
   declarations: [
-    AppComponent, LoginComponent
+    AppComponent, LoginComponent, SignUpComponent
   ],
   imports: [
-    BrowserModule, AppRouting, ShareModule
+    BrowserModule, AppRouting, ShareModule, FormsModule, ReactiveFormsModule, HttpClientModule,
+    CodeInputModule.forRoot({
+      codeLength: 6,
+      isCharsCode: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
