6a38f266b4cdd0c6441e4d2e99de85e2cf8fef63
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2023-03-14 10:41:46 +0700

    update course page

--- projects/main-area/src/app/pages/course/course.componenent.ts
+++ projects/main-area/src/app/pages/course/course.componenent.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from "@angular/platform-browser";
 import { getInitials } from '../../../../../base-area/src/app/utils/getInitial';
 import { Subscription } from "rxjs";
@@ -7,6 +7,8 @@ import { LoginReq } from "projects/base-area/src/app/dto/user/login-req";
 import { UserService } from '../../../../../base-area/src/app/services/user.service';
 import { LoginRes } from '../../../../../base-area/src/app/dto/user/login-res';
 import { Router } from "@angular/router";
+import { CategoryService } from '../../../../../base-area/src/app/services/category.service';
+import { CategoryRes } from '../../../../../base-area/src/app/dto/category/category-res';
 
 
 @Component({
@@ -15,11 +17,32 @@ import { Router } from "@angular/router";
 })
 
 
-export class CourseComponent{
+export class CourseComponent implements OnInit, OnDestroy{
   constructor(private title: Title, private fb: FormBuilder,
-    private userService: UserService,  private router: Router){
+    private userService: UserService,  private router: Router,private categoryService: CategoryService){
       this.title.setTitle("Course")
     }
 
+    private category$?: Subscription
+
+    categories: CategoryRes[] = []
     dateSearch!:Date
+
+    selectedCategory: string[] = []
+
+
+    initCategory(){
+      this.category$ = this.categoryService.getAllCategory().subscribe(res => {
+        this.categories = res;
+      })
+    }
+
+    ngOnInit(): void {
+      this.initCategory()
+    }
+
+    ngOnDestroy(): void {
+     this.category$?.unsubscribe()
+    }
+
 }
