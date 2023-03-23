06e15ff3fd0ac47960f6221a60ac089ffe0ccc55
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2023-03-23 16:04:26 +0700

    addd functionality of like, bookmark and comment

---
+++ projects/base-area/src/app/dto/post/all-post-comment-res.ts
@@ -0,0 +1,8 @@
+export interface PostCommentRes {
+  postCommentId: string,
+  userId: string,
+  fullname: string,
+  contentComment: string,
+  createdAt: string,
+  ver: number
+}
---
+++ projects/base-area/src/app/dto/post/polling-vote-req.ts
@@ -0,0 +1,3 @@
+export interface PollingVoteReqInsert{
+  pollingOptionId: string
+}
