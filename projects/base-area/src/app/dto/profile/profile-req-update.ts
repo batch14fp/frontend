59868a89103a371b5a653fe7ced7b5dad30e6c7a
trushub-capstone <irvandirizky9@gmail.com>
2023-03-23 16:08:55 +0700

    add function profile

--- projects/base-area/src/app/dto/profile/profile-req-update.ts
+++ projects/base-area/src/app/dto/profile/profile-req-update.ts
@@ -1,14 +1,25 @@
+import { SocialMediaGetRes } from "@dto/socialmedia/social-media-res"
+
 export interface ProfileReqUpdate{
-    userId: string,
-    fullname: string,
-    phoneNumber: string,
-    country: string,
-    province: string,
-    city: string,
-    postalCode: string,
-    company: string,
-    industryId: string,
-    poistionId: string,
-    ver : number
-    isActive : boolean
+    userId : string
+	industryId : string
+	positionId : string
+	statusMemberId : string
+	fullname : string
+	userBalance : number
+	statusMember : string
+	phoneNumber : string
+	dob : string
+	country : string
+	province : string
+	city : string
+	postalCode : string
+	company : string
+	imageId : string
+	socialMediaList : SocialMediaGetRes[]
+	ver : number
+	isActive : boolean
 }
+
+// imageContent;
+// imageExtension;
